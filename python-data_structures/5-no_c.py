64e618517eda765ecdc8bb3ae7dbc886cc3d6b4b
i-ganza007 <i.ganza@alustudent.com>
2023-11-09 10:40:20 -0800

    underwood

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-def remove_chars(input_string):
-    # Initialize an empty string to store the result
-    result_string = ""
-
-    # Iterate through each character in the input string
-    for char in input_string:
-        # Check if the character is not 'c' or 'C'
-        if char.lower() not in ['c', 'C']:
-            # Append the character to the result string
-            result_string += char
-
-    return result_string
-original_string = "Best School"
-new_string = remove_chars(original_string)
-print(new_string)
-
+def no_c(my_string):
+    my_string = my_string.translate({ord("c"): None})
+    my_string = my_string.translate({ord("C"): None})
+    return my_string
